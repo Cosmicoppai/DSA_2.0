25de6a32264985b9a2416270b264ab83eef5a303
cosmicoppai <kanakchaudhari12@gmail.com>
2022-05-18 19:30:52 +0530

    add __iter__ and __delitem__ method

--- hash_tables/hash.py
+++ hash_tables/hash.py
@@ -3,36 +3,34 @@ from typing import List, Any
 
 class HashTable:
     def __init__(self, size: int = 1000):
-        self.array = [None] * size
+        self.array = [[] for _ in range(size)]
+        self.length = 0
+        self.size = size
 
     def add(self, key: Any, value: Any) -> None:
-        index = self.compute_index(key)
+        idx = self.compute_index(key)
 
-        if self.array[index]:
-            for kvp in self.array[index]:
-                if kvp[0] == key:
-                    kvp[1] = value
-                    break
-            self.array[index].append((key, value))
+        for kvp in self.array[idx]:
+            if kvp[0] == key:
+                kvp[1] = value
+                break
         else:
-            self.array[index] = [(key, value)]
+            self.array[idx].append([key, value])
+            self.length += 1
 
         if self.is_full():
             self.double_up()
 
     def get(self, key: Any) -> Any:
-        index = self.compute_index(key)
-        if not self.array[index]:
-            raise KeyError
-        for kvp in self.array[index]:
+        idx = self.compute_index(key)
+        for kvp in self.array[idx]:
             if kvp[0] == key:
                 return kvp[1]
 
         raise KeyError
 
     def is_full(self) -> bool:
-        none_items = self.array.count(None)
-        return len(self.array) - none_items > len(self.array) / 2
+        return self.length > self.size / 2
 
     def double_up(self) -> None:
         new_hash_table = HashTable(size=2 * len(self.array))
@@ -45,11 +43,35 @@ class HashTable:
 
         self.array = new_hash_table.array
 
+    def keys(self) -> Any:
+        return self.__iter__()
+
+    def values(self) -> Any:
+        for kvp in self.__iterate_kv():
+            return kvp[1]
+
+    def items(self) -> List[Any]:
+        return self.__iterate_kv()
+
+    def compute_index(self, key: Any) -> int:
+        return hash(key) % self.size
+
     def __getitem__(self, item) -> Any:
         return self.get(item)
 
     def __setitem__(self, key, value) -> Any:
         self.add(key, value)
+
+    def __iterate_kv(self) -> List[Any]:
+        for sub_lst in self.array:
+            if not sub_lst:
+                continue
+            for kvp in sub_lst:
+                yield kvp
+
+    def __iter__(self) -> Any:
+        for kvp in self.__iterate_kv():
+            yield kvp[0]
 
     def __contains__(self, item) -> bool:
         try:
@@ -58,7 +80,38 @@ class HashTable:
         except KeyError:
             return False
 
-    def __eq__(self, other: HashTable) -> bool:
+    def __eq__(self, other: object) -> bool:
         return self.array == other.array
 
+    def __len__(self):
+        return self.length
+
+    def __delitem__(self, key: Any) -> None:
+        idx = self.compute_index(key)
+        for kvp in self.array[idx]:
+            if kvp[0] == key:
+                self.array[idx].remove(kvp)
+                self.length -= 1
+                return
+
+        raise KeyError("Key {} doesn't exist".format(key))
+
     __hash__ = None
+
+
+if __name__ == "__main__":
+    hash_table = HashTable()
+    hash_table.add(1, "cosmicoppai")
+    hash_table.add(2, "seo_ye_ji")
+    hash_table.add(3, "hana")
+
+    print(len(hash_table))
+    print(1 in hash_table)
+
+    for key in hash_table:
+        print(key)
+
+    hash_table[4] = "tsubasa"
+    print(len(hash_table))
+    del hash_table[1]
+    print(len(hash_table))
